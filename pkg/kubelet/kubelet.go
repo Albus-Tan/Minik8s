b0b75d9d564c7eb47d68d70517124f55a87af7f5
jiarui <wangjiarui4@outlook.com>
2023-05-12 10:34:01 +0800

    feat: remove cri client form pod manager

--- pkg/kubelet/kubelet.go
+++ pkg/kubelet/kubelet.go
@@ -10,10 +10,7 @@ import (
 	"minik8s/pkg/apiclient"
 	client "minik8s/pkg/apiclient/interface"
 	"minik8s/pkg/apiclient/listwatch"
-	"minik8s/pkg/kubelet/constants"
-	"minik8s/pkg/kubelet/container/cri"
 	"minik8s/pkg/kubelet/pod"
-	"sync"
 )
 
 type Kubelet interface {
@@ -28,29 +25,15 @@ func New() (Kubelet, error) {
 		return nil, err
 	}
 
-	criClient, err := cri.NewDocker()
-	if err != nil {
-		return nil, err
-	}
-
 	return &kubelet{
 		name:             "Kubelet", // FIXME: change to node name + Kubelet
 		podClient:        podClient,
 		podListerWatcher: listwatch.NewListWatchFromClient(podClient),
 		podManager:       pod.NewPodManager(),
-		criClient:        criClient,
-		pcfLock:          sync.RWMutex{},
-		podCancelFunc:    make(map[string]context.CancelFunc),
-		podCancelWG:      make(map[string]*sync.WaitGroup),
 	}, nil
 }
 
 func (k *kubelet) Close() {
-	for _, c := range k.podCancelFunc {
-		c()
-	}
-
-	k.criClient.Close()
 }
 
 type kubelet struct {
@@ -58,11 +41,6 @@ type kubelet struct {
 	podClient        client.Interface
 	podListerWatcher listwatch.ListerWatcher
 	podManager       pod.Manager
-	criClient        cri.CriClient
-
-	pcfLock       sync.RWMutex
-	podCancelFunc map[string]context.CancelFunc
-	podCancelWG   map[string]*sync.WaitGroup
 }
 
 func (k *kubelet) Run() {
@@ -150,55 +128,23 @@ loop:
 }
 
 func (k *kubelet) handlePodCreate(pod *core.Pod) {
-	k.pcfLock.Lock()
-	defer k.pcfLock.Unlock()
-
-	// install Initial Containers
-	pod.Spec.InitContainers = append(pod.Spec.InitContainers, constants.InitialPauseContainer)
-
-	ctx, cancel := context.WithCancel(context.Background())
-	wg := &sync.WaitGroup{}
-
-	for _, c := range pod.Spec.InitContainers {
-		wg.Add(1)
-		go k.criClient.ContainerEnsure(ctx, c, wg)
-	}
-
-	for _, c := range pod.Spec.Containers {
-		wg.Add(1)
-		go k.criClient.ContainerEnsure(ctx, c, wg)
-	}
 	// FIXME: update container status field in pod
 
 	// FIXME: update pod status
 
 	// FIXME: send new pod status back to apiserver
-	// register cancel function
-	k.podCancelFunc[pod.UID] = cancel
-	k.podCancelWG[pod.UID] = wg
 	// add pod to podManager
 	k.podManager.AddPod(pod)
 }
 
 func (k *kubelet) handlePodModify(pod *core.Pod) {
-	k.handlePodDelete(pod)
-	k.handlePodCreate(pod)
 }
 
 func (k *kubelet) handlePodDelete(pod *core.Pod) {
-	k.pcfLock.Lock()
-	defer k.pcfLock.Unlock()
 	// FIXME: update field in pod
 
 	// FIXME: send new pod status back to apiserver
-	// call cancel function to delete containers
-	if cancel := k.podCancelFunc[pod.UID]; cancel == nil {
-		log.Println("[ERROR] delete uncreated pod")
-	} else {
-		cancel()
-		wg := k.podCancelWG[pod.UID]
-		wg.Wait()
-	}
+
 	// delete pod in podManager
 	k.podManager.DeletePod(pod)
 }
