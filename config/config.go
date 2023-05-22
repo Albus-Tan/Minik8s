c0c24612da258fdf06f35642dd3418b37de52c25
jiarui <wangjiarui4@outlook.com>
2023-05-22 15:07:51 +0800

    feat: modify hb time period

--- config/config.go
+++ config/config.go
@@ -56,6 +56,6 @@ const (
 )
 
 /*--------------- Heartbeat ---------------*/
-const HeartbeatInterval = time.Duration(1) * time.Second
-const HeartbeatDeadInterval = time.Duration(15) * time.Second
-const HeartbeatCheckInterval = time.Duration(3) * time.Second
+const HeartbeatInterval = time.Duration(3) * time.Second
+const HeartbeatDeadInterval = time.Duration(30) * time.Second
+const HeartbeatCheckInterval = time.Duration(5) * time.Second
